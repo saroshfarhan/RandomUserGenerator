f7a430b63db19c35dd715b4efe4282d56241c3d8
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-10-16 01:13:33 +0530

    Added randomized babckground color

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ let getUser = () => {
             outerDiv.innerHTML =`<h3 class="name">${data.first_name} ${data.last_name}</h3>
             <p class="title">${data.employment.title}</p>
             <p class="description">${data.email} ﹒ <strong>@${data.username}</strong></p>`;
+
+            let randomColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+        document.documentElement.style.setProperty("--theme-color", randomColor);
         });
+
+        
 };
 
 window.addEventListener("load", getUser);
